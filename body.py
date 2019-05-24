64ac544635055a1d8f6eaa92d00e47c3e0a0266c
KelvinAstro <44537466+KelvinAstro@users.noreply.github.com>
2019-05-24 10:33:41 -0400

    finished??

--- body.py
+++ body.py
@@ -5,12 +5,14 @@ import matplotlib.animation as animation
 
 
 class Body:
-    def __init__(self, radius, angle=0, distance=0, color="y", satellites=None):
+    def __init__(self, radius, angle=0, distance=0, days=1, color="y", satellites=None):
         """Class for creating a celestial body and its satellites (e.g., the sun, planets, moons). 
         radius: the radius of the body
         angle: the angle around the orgin of the graph.
 
         distance: the distace the body is from its parent. Zero if it has no parent (e.g. the sun).
+
+        orbit: earth days to complete an orbit
         
         color: the body's color. rgb in range [0, 1]; hex string; 
         {'b', 'g', 'r', 'c', 'm', 'y', 'k', 'w'}; or any othe matplotlib formats
@@ -20,46 +22,54 @@ class Body:
         and asteroids that orbit it.
         """
         self.distance = distance
-        self.radius = radius/10
-        self.angle = angle 
+        self.radius = radius / 2
+        self.angle = angle
         self.color = color
         self.x = distance * np.cos(np.radians(angle))
         self.y = distance * np.sin(np.radians(angle))
+        self.angularSpeed = (2 * np.pi) / (days)
 
         if satellites is None:
             self.satellites = np.array([], dtype=Body)
         else:
-            for sat in satellites:
-                sat.x = sat.distance * np.cos(np.radians(sat.angle) + np.radians(self.angle)) + self.x
-                sat.y = sat.distance * np.sin(np.radians(sat.angle) + np.radians(self.angle)) + self.y
+            satellites = self.satTranslate(satellites)
             self.satellites = np.array(satellites)
 
-        res = 10
+        res = 5
         self.u = np.linspace(0, 2 * np.pi, res)
         self.v = np.linspace(0, np.pi, res)
+
+    def satTranslate(self, satList):
+        for sat in satList:
+            sat.x = (
+                sat.distance * np.cos(np.radians(sat.angle) + np.radians(self.angle))
+                + self.x
+            )
+            sat.y = (
+                sat.distance * np.sin(np.radians(sat.angle) + np.radians(self.angle))
+                + self.y
+            )
+        return satList
 
     def sphere(self):
         x = self.radius * np.outer(np.cos(self.u), np.sin(self.v)) + self.x
         y = self.radius * np.outer(np.sin(self.u), np.sin(self.v)) + self.y
-        z = self.radius * np.outer(np.ones(np.size(self.u)), np.cos(self.v))
+        z = (self.radius/5) * np.outer(np.ones(np.size(self.u)), np.cos(self.v))
 
         return x, y, z
 
     def creatSatellites(self, satList):
-        for sat in satList:
-            sat.x = sat.distance * np.cos(np.radians(sat.angle)  + np.radians(self.angle)) + self.x
-            sat.y = sat.distance * np.sin(np.radians(sat.angle)  + np.radians(self.angle)) + self.y
+        satList = self.satTranslate(satList)
         self.satellites = np.append(self.satellites, satList)
 
-    def move(self, angle):
-        print("ok")
-        self.angle += angle 
+    def orbit(self, angle, x=0, y=0):
+        self.angle += angle
+        self.angle = self.angle % 360
         oldX, oldY = self.x, self.y
-        self.x = self.distance * np.cos(np.radians(self.angle))
-        self.y = self.distance * np.sin(np.radians(self.angle))
+        self.x = self.distance * np.cos(np.radians(self.angle)) + x
+        self.y = self.distance * np.sin(np.radians(self.angle)) + y
         for sat in self.satellites:
+            sat.orbit((1/sat.angularSpeed) + angle, self.x, self.y)
             sat.x += self.x - oldX
             sat.y += self.y - oldY
-
-
 
--- planet.py
+++ planet.py
@@ -8,68 +8,64 @@ from body import Body
 def plot(body):
     x, y, z = body.sphere()
     color = body.color
-#     ax.plot_surface(x, y, z, color=color)
-    ax.scatter(body.x, body.y, zs=0, zdir="y", c=color, label="points in (x,z)")
-    for i in body.satellites:
-        plot(i)
+    ax.plot_surface(x, y, z, color=color)
+
+    for sat in body.satellites:
+        plot(sat)
 
 
 def animate(angle, bodys):
+    init()
     for body in bodys:
-        body.move(angle)
+        body.orbit(np.rad2deg(body.angularSpeed))
         plot(body)
-        print(body, body.angle * 180 / np.pi)
 
 
-# Fixing random state for reproducibility
-np.random.seed(14537)
+def init():
+    # Setting the axes properties
+    ax.clear()
+    x, y, z = sun.sphere()
+    color = sun.color
+    ax.plot_surface(x, y, z, color=color)
+    dim = 40
+    ax.set_xlim3d([-dim, dim])
+    ax.set_xlabel("X")
+
+    ax.set_ylim3d([-dim, dim])
+    ax.set_ylabel("Y")
+
+    ax.set_zlim3d([-5, 5])
+    ax.set_zlabel("Z")
+
+    ax.set_title("3D Test")
+
+
+# creating the sun, planets and moon
+moon = Body(1, 90, 10, 28, color="k")
+earthSats = [moon]
+
+sun = Body(10)
+mercury = Body(3, 90, 10, 88, color="#6f1520")
+venus = Body(3, 90, 15, 224.7, color="#c46b00")
+earth = Body(3, 90, 20, 365.3, satellites=earthSats, color="#0079c4")
+mars = Body(3, 90, 25, 687, color="#9e2111")
+jupiter = Body(3, 90, 4331.6, color="#d89741")
+saturn = Body(3,90,30, 10759.2, color="#e0c198")
+
+sunSats = [mercury, venus, earth, mars, jupiter, saturn]
+sun.creatSatellites(sunSats)
+
 
 
 # Attaching 3D axis to the figure
 fig = plt.figure()
-ax = p3.Axes3D(fig, azim=90, elev=0, proj_type="ortho")
-
-# Fifty lines of random 3-D lines
-
-# Creating fifty line objects.
-# NOTE: Can't pass empty arrays into 3d version of plot()
-
-# Setting the axes properties
-ax.set_xlim3d([-20.0, 20.0])
-ax.set_xlabel("X")
-
-ax.set_ylim3d([-20.0, 20.0])
-ax.set_ylabel("Y")
-
-ax.set_zlim3d([-20.0, 20.0])
-ax.set_zlabel("Z")
-
-ax.set_title("3D Test")
+ax = p3.Axes3D(fig, azim=90, elev=0, proj_type="persp")
 
 
-# creating the sun, planets and moon
-moon = Body(1, 90, 10, "k")
-earthSats = [moon]
-sun = Body(10)
-earth = Body(3, 90, 20, satellites=earthSats, color="b")
-mars = Body(6, 90, 25, "r")
+# Animation
 
-
-sun.creatSatellites([earth])
-sun.creatSatellites([mars])
-
-
-# Plot the surface
-
-plot(sun)
-x = 1
-planets = [[earth, mars]]
-ani = animation.FuncAnimation(fig, animate, [5], fargs=(planets), interval=10)
-
-print(earth.x, earth.y)
-print(moon.x, moon.y)
-print(np.sqrt(np.square(earth.x - moon.x) + np.square(earth.y - moon.y)))
-print((np.arctan((earth.y - moon.y) / (earth.x - moon.x)) * 180 / np.pi))
+ani = animation.FuncAnimation(
+    fig, animate, [5], fargs=([sun.satellites]), interval=1
+)
 
 plt.show()
-
